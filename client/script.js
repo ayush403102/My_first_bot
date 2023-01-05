a4b4519f5f36415d255aa135775a84db467eefe0
Ayush Agarwal <ayush403102@gmail.com>
2023-01-05 19:54:23 +0530

    update  the server url

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async(e) => {
 
   //fetch data from server => bot's responce
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codebot-iya9.onrender.com', {
     method:'POST',
     headers: {
       'Content-Type': 'application/json'
